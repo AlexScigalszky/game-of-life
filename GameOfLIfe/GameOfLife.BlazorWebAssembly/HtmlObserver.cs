bc6846b6b04fbdda4e87c7181c2f95542bbb6d01
Alex P. Scigalszky <alexpablo.90@gmail.com>
2022-11-25 11:56:14 -0300

    add blazor web assembly

---
+++ GameOfLIfe/GameOfLife.BlazorWebAssembly/HtmlObserver.cs
@@ -0,0 +1,52 @@
+﻿using Engine.Interfaces;
+
+namespace GameOfLife.BlazorWebAssembly
+{
+    public class HtmlObserver : IGameObserver
+    {
+        private int _width = 5;
+        private int _step = 0;
+        public string Logger { get; private set; } = "";
+
+        public HtmlObserver(int width)
+        {
+            _width = width;
+        }
+
+        public void Update(IEnumerable<ICell> cells)
+        {
+            var newStep = $"Print step {_step}" + "\n";
+
+            var str = "|";
+            for (var j = 0; j < _width - 1; j++)
+            {
+                str += "----";
+            }
+            newStep += str + "\n";
+            for (var i = 0; i < _width - 1; i++)
+            {
+                str = "|";
+                for (var j = 0; j < _width - 1; j++)
+                {
+                    str += Print(cells.ElementAt(i * j + 1)) + "|";
+                }
+                newStep += str + "\n";
+
+                str = "|";
+                for (var j = 0; j < _width - 1; j++)
+                {
+                    str += "----";
+                }
+                newStep += str + "\n";
+            }
+
+            Logger = newStep + "\n" + Logger;
+            _step++;
+        }
+
+        private string Print(ICell cell)
+        {
+            return cell.CurrentState.State == "Alive" ? " 0 " : "   ";
+        }
+    }
+}
---
+++ GameOfLIfe/GameOfLife.BlazorWebAssembly/SquareLandReseatable.cs
@@ -0,0 +1,19 @@
+﻿using Engine.Implementations;
+using Engine.Interfaces;
+
+namespace GameOfLife.BlazorWebAssembly
+{
+    public class SquareLandReseatable : SquareLand
+    {
+        public SquareLandReseatable(int width) : base(width)
+        {
+        }
+
+        public void Reset()
+        {
+            _nextAvailableSpace = 0;
+            _grid = new ICell[_maxAvailableSpace];
+            _indexes = new Dictionary<Guid, int>();
+        }
+    }
+}
